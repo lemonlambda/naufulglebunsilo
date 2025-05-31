d73dcf98d4c87d93ac98335e7a2dff2f45c17c3d
lambdalemon <lemonjameslem@protonmail.com>
2025-05-30 23:01:02 -0400

    2.0.0

--- prototype/ammonia_tile.lua
+++ prototype/ammonia_tile.lua
@@ -1,5 +1,6 @@
 local ammonia_lava = table.deepcopy(data.raw["tile"]["ammoniacal-ocean"])
 ammonia_lava.name = "ammonia-lava"
 ammonia_lava.autoplace = data.raw["tile"]["lava"].autoplace
+table.insert(data.raw["item"]["ice-platform"].place_as_tile.tile_condition, "ammonia-lava");
 
 data:extend{ammonia_lava}
